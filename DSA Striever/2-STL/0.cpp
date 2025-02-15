ba200e3c67e6b91746debdf53651eb41e10afc9f
RyomenDev <aakashvatsh007@gmail.com>
2025-02-15 19:51:31 +0530

    stl

--- DSA Striever/2-STL/0.cpp
+++ DSA Striever/2-STL/0.cpp
@@ -1,4 +1,56 @@
 #include<bits/stdc++.h> // std library
+/*
+Alogrithms
+    - sort, binary_search, next_permutation, lower_bound, upper_bound
+Containers
+    - vector, set, map, stack, queue, priority_queue, list, deque
+Iterators
+    - begin(), end(), rbegin(), rend()
+    - advance(), next(), prev()
+    - inserter(), back_inserter(), front_inserter()
+    - distance()
+    - make_move_iterator(), make_reverse_iterator()
+    - istream_iterator(), ostream_iterator()
+    - move(), forward()
+    - back_inserter(), front_inserter(), inserter()
+    - istream_iterator(), ostream_iterator()
+    - make_move_iterator(), make_reverse_iterator()
+    - move(), forward()
+    - back_inserter(), front_inserter(), inserter()
+    - istream_iterator(), ostream_iterator()
+    - make_move_iterator(), make_reverse_iterator()
+    - move(), forward()
+    - back_inserter(), front_inserter(), inserter()
+    - istream_iterator(), ostream_iterator()
+    - make_move_iterator(), make_reverse_iterator()
+    - move(), forward()
+    - back_inserter(), front_inserter(), inserter()
+    - istream_iterator(), ostream_iterator()
+    - make_move_iterator(), make_reverse_iterator()
+    - move(), forward()
+    - back_inserter(), front_inserter(), inserter()
+    - istream_iterator(), ostream_iterator()
+    - make_move_iterator(), make_reverse_iterator()
+    - move(), forward()
+    - back_inserter(), front_inserter(), inserter()
+    - istream_iterator(), ostream_iterator()
+    - make_move_iterator(), make_reverse_iterator()
+    - move(), forward()
+    - back_inserter(), front_inserter(), inserter()
+    - istream_iterator(), ostream_iterator()
+    - make_move_iterator(), make_reverse_iterator()
+    - move(), forward()
+    - back_inserter(), front_inserter(), inserter()
+    - istream_iterator(), ostream_iterator()
+    - make_move_iterator(), make_reverse_iterator()
+    - move(), forward()
+    - back_inserter(), front_inserter(), inserter()
+    - istream_iterator(), ostream_iterator()
+    - make_move_iterator(), make_reverse_iterator()
+    - move(), forward()
+Functions
+    - sort, binary_search, next_permutation, lower_bound, upper_bound
+*/
 using namespace std;
 
 int32_t main()
