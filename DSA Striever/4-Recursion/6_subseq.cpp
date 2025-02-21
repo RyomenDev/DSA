764ee59e4ef90a1e4813a097411893368d51e81f
RyomenDev <aakashvatsh007@gmail.com>
2025-02-21 17:59:41 +0530

    recusion-subseq

--- DSA Striever/4-Recursion/6_subseq.cpp
+++ DSA Striever/4-Recursion/6_subseq.cpp
@@ -1,5 +1,5 @@
 
-// subsequences : contiguous /non-contiguous, follow order
+// subsequences1 : contiguous /non-contiguous, follow order
 // subArray : contiguous,order matter
 
 // time complexity : O(2^n*n)
@@ -7,19 +7,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 // print all subsequences
-// void subsequences(string s,string ans)
-// {
-//     if(s.length()==0)
-//     {
-//         cout<<ans<<" ";
-//         return;
-//     }
-//     char ch=s[0];
-//     string ros=s.substr(1);
-//     subsequences(ros,ans+ch);
-//     subsequences(ros,ans);
-// }
-void subsequences(int index, vector<int> ans, vector<int> v)
+void subsequences(string s, string ans)
+{
+    if (s.length() == 0)
+    {
+        cout << ans << " ";
+        return;
+    }
+    char ch = s[0];
+    string ros = s.substr(1);
+    subsequences(ros, ans + ch);
+    subsequences(ros, ans);
+}
+
+void subsequences1(int index, vector<int> ans, vector<int> v)
 {
     int n = v.size();
     if (index >= n)
@@ -30,15 +31,49 @@ void subsequences(int index, vector<int> ans, vector<int> v)
         return;
     }
     ans.push_back(v[index]);
-    subsequences(index + 1, ans, v);
+    subsequences1(index + 1, ans, v);
     ans.pop_back();
-    subsequences(index + 1, ans, v);
+    subsequences1(index + 1, ans, v);
+}
+
+// void subsequences2(int index, vector<int> ans, vector<int> v)
+// {
+//     int n = v.size();
+//     if (index >= n)
+//     {
+//         for (auto x : ans)
+//             cout << x << " ";
+//         cout << endl;
+//         return;
+//     }
+//     subsequences2(index + 1, ans, v);
+//     ans.push_back(v[index]);
+//     subsequences2(index + 1, ans, v);
+//     ans.pop_back();
+// }
+
+
+void subsequences3(int index, vector<int> ans, vector<int> v)
+{
+    if (index < 0)
+    {
+        for (auto x = rbegin(ans); x != rend(ans); x++)
+            cout << *x << " ";
+        cout << endl;
+        return;
+    }
+    subsequences3(index - 1, ans, v);
+    ans.push_back(v[index]);
+    subsequences3(index - 1, ans, v);
+    ans.pop_back();
 }
 
 int main()
 {
-    // subsequences(0, "abcd", "");
+    subsequences("abcd", "");
     vector<int> v = {1, 2, 3};
-    subsequences(0, {}, v);
+    subsequences1(0, {}, v);
+    // subsequences2(0, {}, v);
+    subsequences3(v.size() - 1, {}, v);
     return 0;
 }
