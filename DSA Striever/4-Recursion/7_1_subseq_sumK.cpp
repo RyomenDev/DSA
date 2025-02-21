b98cc50a40aa85aac10e4f5c7ce1fd6de5402f32
RyomenDev <aakashvatsh007@gmail.com>
2025-02-21 22:04:57 +0530

    sum==k

--- DSA Striever/4-Recursion/7_1_subseq_sumK.cpp
+++ DSA Striever/4-Recursion/7_1_subseq_sumK.cpp
@@ -9,7 +9,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool subsequences(int index, vector<int> ans, vector<int> v, int k, int &sum)
+bool subsequences(int index, vector<int> ans, vector<int> v, int k, int& sum)
 {
     int n = v.size();
     if (index >= n)
@@ -35,10 +35,33 @@ bool subsequences(int index, vector<int> ans, vector<int> v, int k, int &sum)
     return false;
 }
 
+void subsequences1(int index, vector<int> ans, vector<int> v, int k, int& sum)
+{
+    int n = v.size();
+    if (index >= n)
+    {
+        if (sum == k)
+        {
+            for (auto x : ans)
+                cout << x << " ";
+            cout << endl;
+        }
+        return;
+    }
+    ans.push_back(v[index]);
+    // sum += v[index];
+    subsequences1(index + 1, ans, v, k, sum += v[index]);
+    ans.pop_back();
+    // sum -= v[index];
+    subsequences1(index + 1, ans, v, k, sum -= v[index]);
+    // return;
+}
+
 int main()
 {
     vector<int> v = {1, 2, 3, 4, 2};
     int k = 5, sum = 0;
-    subsequences(0, {}, v, k, sum);
+    // subsequences(0, {}, v, k, sum);
+    subsequences1(0, {}, v, k, sum);
     return 0;
 }
