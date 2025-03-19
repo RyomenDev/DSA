048e847b9556cca1bb5b51480cb5e3bc7cfdc74c
RyomenDev <aakashvatsh007@gmail.com>
2025-03-19 09:26:32 +0530

    Bottom View of Binary Tree

---
+++ DSA Striever/6-Tree/23_bottomView.cpp
@@ -0,0 +1,26 @@
+https://www.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+
+class Solution {
+public:
+    vector <int> bottomView(Node* root) {
+        // Your Code Here
+        vector<int> ans;
+        if (!root) return ans;
+        map<int, int>mp;
+        queue<pair<Node*, int>>q; // {node, horizontalDistance} node , rowNum
+        q.push({root,0});
+        while (!q.empty()) {
+            Node* curr = q.front().first;
+            int rowNum = q.front().second; // Horizontal distance
+            q.pop();
+            // if(mp.find(rowNum)==mp.end()) 
+            mp[rowNum] = curr->data;
+            if (curr->left) q.push({curr->left,rowNum - 1});
+            if (curr->right) q.push({curr->right,rowNum + 1});
+        }
+        for (auto it = mp.begin(); it != mp.end(); ++it)
+            ans.push_back(it->second);
+        mp.clear();
+        return ans;
+    }
+};
